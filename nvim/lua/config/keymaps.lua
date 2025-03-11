a4bad3acb0b0574aecdde1df740577b0de60ce99
holtom <yy@ml-yyy1.taichisgn4.local>
2025-03-11 16:59:44 +0800

    first commit

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,19 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+-- 设置主键
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- 单行或多行移动
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Telescope 搜索插件配置
+local builtin = require("telescope.builtin")
+keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+keymap.set("n", "<leader>fg", builtin.grep_string, { desc = "Telescope grep string" })
+keymap.set("n", "<leader>fw", builtin.live_grep, { desc = "Telescope live grep" })
+keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
